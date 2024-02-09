3a3306fb309c679735d2ef647997d792e3924ccf
broncogooch <calebgooch2000@gmail.com>
2024-02-09 16:55:30 -0700

    Add project files.

---
+++ Mission5Assignment/Controllers/LessonsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission5Assignment.Models;
+using System.Diagnostics;
+
+namespace Mission5Assignment.Controllers
+{
+    public class LessonsController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public LessonsController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Lessons()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mission5Assignment/wwwroot/js/site.js
@@ -0,0 +1,21 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(document).ready(function () {
+    $('#calculate').click(function () {
+        var hours = parseFloat($('#hours').val());
+
+        // Validation to make sure a positive number is entered
+        if (isNaN(hours) || hours <= 0) {
+            alert("Please enter a valid positive number for hours.");
+            return;
+        }
+
+        var charge = parseFloat($('#charge').val());
+        var total = hours * charge;
+
+        // Display total
+        $('#total').text('$' + total.toFixed(2)); // Assuming you're using USD currency
+    });
+});
